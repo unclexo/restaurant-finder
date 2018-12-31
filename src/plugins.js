6f67ed07c9ecfbc36736e5358adc8089d8a9c96e
#unclexo <abujobaer@gmail.com>
2018-12-31 16:46:13 +0600

    Fixes accessing env vars

--- src/plugins.js
+++ src/plugins.js
@@ -7,14 +7,14 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 // Sets defaults
 axios.defaults.baseURL = 'https://developers.zomato.com/api/v2.1'
-axios.defaults.headers.common['Accept'] = 'application/json'
-axios.defaults.headers.common['user-key'] = process.env.ZOMATO_API_KEY
+axios.defaults.headers.common['Accept'] = "application/json"
+axios.defaults.headers.common['user-key'] = process.env.VUE_APP_ZOMATO_API_KEY
 
 // Registers plugins
 Vue.use(Vuex)
 Vue.use(Vuetify)
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.GOOGLE_API_KEY || ''
+    key: process.env.VUE_APP_GOOGLE_API_KEY || ''
   }
 })
