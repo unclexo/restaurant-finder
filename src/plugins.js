8de1dd8481dc3efa6f87a71eced7aee228d979e8
#unclexo <abujobaer@gmail.com>
2019-01-01 17:33:49 +0600

    Shortens key consts

--- src/plugins.js
+++ src/plugins.js
@@ -8,13 +8,13 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 // Sets defaults
 axios.defaults.baseURL = 'https://developers.zomato.com/api/v2.1'
 axios.defaults.headers.common['Accept'] = "application/json"
-axios.defaults.headers.common['user-key'] = process.env.VUE_APP_ZOMATO_API_KEY
+axios.defaults.headers.common['user-key'] = process.env.VUE_APP_ZOMATO_KEY
 
 // Registers plugins
 Vue.use(Vuex)
 Vue.use(Vuetify)
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.VUE_APP_GOOGLE_MAP_API_KEY || ''
+    key: process.env.VUE_APP_GOOGLE_MAP_KEY || ''
   }
 })
